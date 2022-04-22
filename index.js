30ffab069e1327dbeee6dc3ea0f8774a5374f0bc
RishitV9 <varshneyrishit5@protonmail.com>
2022-04-22 17:26:12 +0000

    Working

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const fs = require("fs")
 var unblocker = Unblocker({});
 
 var server = http.createServer(function (req, res) {
-    // first let unblocker try to handle the requests
     unblocker(req, res, function (err) {
-      // this callback will be fired for any request that unblocker does not serve
       var headers = { "content-type": "html" };
 
       if (err) {
@@ -27,7 +25,6 @@ var server = http.createServer(function (req, res) {
   })
   .listen(8080);
 
-// allow unblocker to proxy websockets
 server.on("upgrade", unblocker.onUpgrade);
 
 console.log("proxy server live at http://localhost:8080/");
