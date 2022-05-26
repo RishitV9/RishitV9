c4bbf39d8b3b1a7ac631bafca29684d5e7fafbec
RishitV9 <varshneyrishit5@protonmail.com>
2022-05-26 01:05:12 +0000

    jfgierug

--- pxy/index.js
+++ pxy/index.js
@@ -3,15 +3,32 @@ const Unblocker = require("unblocker");
 const fs = require("fs");
 const https = require("http");
 const unblocker = Unblocker({});
+const txtLog = new console.Console(fs.createWriteStream('logs.txt'));
 const port = 8080;
+const headers = { "content-type": "html" };
+
+// ip address funstion:
+const parseIp = (req) =>
+    req.headers['x-forwarded-for']?.split(',').shift()
+    || req.socket?.remoteAddress
 
 
 // startup:
 console.log(`[*] STARTING AT PORT: ${port}....`);
 const server = https.createServer(function (req, res) {
-    console.log()
+    if (req.url === "/log/") {
+      res.writeHead(200, headers);
+      fs.readFile("logs.txt", "utf-8", (error, data) => {
+        if (!error) {
+          return res.end(data);
+        }
+      })
+    }
+    if (!req.url.indexOf("/u7hhgw0lh2.herokuapp.com/socket.io/")) {
+      txtLog.log("[*] GET -> REQUESTED: (", req.url, ") BY " + parseIp(req))
+    }
     unblocker(req, res, function (err) {
-      const headers = { "content-type": "html" };
+
 
       // send response
       if (err) {
@@ -20,6 +37,7 @@ const server = https.createServer(function (req, res) {
       }
 
       else {
+        txtLog.log("[*] INVALID URL, SENDING block.html...")
         res.writeHead(200, headers);
         fs.readFile("block.html", "utf-8", (error, data) => {
             if (!error) {
@@ -33,4 +51,4 @@ const server = https.createServer(function (req, res) {
 
 server.on("upgrade", unblocker.onUpgrade);
 
-console.log(`[*] Proxy Server live at http://${port}-rishitv9-rishitv9-ldgqgowvow7.ws-us45.gitpod.io/`);
+console.log(`[*] Proxy Server live at http://localhost:${port} (use gitpod link for usage)`);
