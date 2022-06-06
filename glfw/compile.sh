f6b67f407482a9f56d1cbfe644feced953137326
RishitV9 <varshneyrishit5@protonmail.com>
2022-06-06 17:42:11 +0000

    jishnu lilaji

---
+++ glfw/compile.sh
@@ -0,0 +1,3 @@
+g++ main.cpp -o main -lopengl32 -lgdi32 -lglfw3
+./main
+rm main
---
+++ glfw/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "INDIA IS THE MOST DEVINE PLACE!" << endl;
+}
